9b0cad0446bca563a3b273c3c9e2e14832c639a0
NimeshGhag <ghagnimesh490@gmail.com>
2026-01-07 12:27:16 +0530

    feat: implement login with jwt stored in cookies

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -37,16 +37,47 @@ const registerController = async (req, res) => {
 
   res.status(201).json({
     message: "User Register Successfully",
-    user:{
-        email:user.email,
-        fullName:user.fullName
-    }
+    user: {
+      email: user.email,
+      fullName: user.fullName,
+    },
   });
-  
 };
 
 // Login controller
-const loginController = async (req, res) => {};
+const loginController = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({
+    email,
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "User Dosen't exists",
+    });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid Password",
+    });
+  }
+
+  const token = JWT.sign({ user: user._id }, process.env.JWT_SECRET);
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "User Login Successfully",
+    user: {
+      email: user.email,
+      fullName: user.fullName,
+    },
+  });
+};
 
 module.exports = {
   registerController,
