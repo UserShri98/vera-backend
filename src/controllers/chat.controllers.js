99aee16d3492b4649c6bfeeb7838cf2956801ce1
NimeshGhag <ghagnimesh490@gmail.com>
2026-01-07 13:14:04 +0530

    feat: implement authenticated chat creation

--- src/controllers/chat.controllers.js
+++ src/controllers/chat.controllers.js
@@ -1,4 +1,19 @@
-const createChatController = async (req, res) => {};
+const chatModel = require("../models/chat.model")
+
+const createChatController = async (req, res) => {
+  const { title } = req.body;
+  const user = req.user;
+
+  const chat = await chatModel.create({
+    user:user._id,
+    title:title
+  })
+
+  res.status(201).json({
+    status: "Chat created successfully",
+    chat: chat,
+  });
+};
 
 module.exports = {
   createChatController,
