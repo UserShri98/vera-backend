2b885cd2ea1cd0e43b08c0f65b7056cc35c0c923
UserShri98 <sjc3098@gmail.com>
2026-02-05 19:27:43 +0530

    added origin in cors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.VITE_FRONTEND_URL,
+    origin: "https://vera-frontend-seven.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
