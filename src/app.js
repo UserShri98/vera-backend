808361ff09901ca27469dbfc8e401d2c2488e09c
UserShri98 <sjc3098@gmail.com>
2026-02-05 17:20:02 +0530

    changed cors headers

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use(
   cors({
     origin: process.env.VITE_FRONTEND_URL,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
